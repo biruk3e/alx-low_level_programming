b73dd514db6604b168e8bbd6feb1934ad5fba5dc
biruk <you@example.com>
2022-07-01 04:43:42 -0700

    new

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - print whether the number n is +ve or -ve */
-
-/* Return: always 0 */
+/**
+ * main - print whether the number n is +ve or -ve
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	int (n);
