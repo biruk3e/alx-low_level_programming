40a7baa0549585150b79eec541a10e8eb26c2486
biruk <you@example.com>
2022-07-01 13:09:27 -0700

    alpha

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - print whether the number n is +ve or -ve */
-
-/* Return: always 0 */
+/* main - print whether the number n is +ve or -ve
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	if (n > 5)
 	{
-		printf("%d is positive\n", n);
+		printf("the last digit of %d is greater than 5\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("the last digit of %d is zero\n", n);
 	}
-	else
+	else if (n < 6; n!=0)
 	{
-		printf("%d is negative\n", n);
-	}
+		printf("the last digit of %d and is less than 6 and not 0\n", n)	}
 	return (0);
 }
