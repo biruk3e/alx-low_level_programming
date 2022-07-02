ec4ff5f05c916f762fe9a418875ac6620d862511
biruk <you@example.com>
2022-07-02 00:26:40 -0700

    z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - counts to 10
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0' ; i < '9'; i++)
+	{
+	putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
