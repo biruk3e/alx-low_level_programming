74300b64b744ffd57c227c8dffef2bba92060d35
biruk <you@example.com>
2022-07-01 23:50:32 -0700

    base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-	for (char hex = '1'; hex <= '9'; hex++)
+	char hex;
+
+	for (hex = '1'; hex <= '9'; hex++)
 	{
 	putchar(hex);
 	}
-	for (char hex = 'a'; hex <= 'f'; hex++)
+	for (hex = 'a'; hex <= 'f'; hex++)
 	{
 	putchar(hex);
 	}
