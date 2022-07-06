815737bfd5d5f6bb12f21bdb8926e92914735e4a
biruk <you@example.com>
2022-07-06 04:46:02 -0700

    3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_10x - prints alphabet
+ *
  * Return: always 0
  */
 void print_alphabet_10x(void)
@@ -13,8 +14,22 @@ void print_alphabet_10x(void)
 	int j;
 
 	for (j = 0; j < 26; j++)
+	{
 	_putchar(character);
 	character++;
 	}
 	_putchar('\n');
+	}
 }
+/**
+ * main - prints main
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet_10x();
+	return (0);
+}
+
+
