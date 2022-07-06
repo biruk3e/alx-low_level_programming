5bb47ba249e32bfe1fcdb86926337dd13059eed7
biruk <you@example.com>
2022-07-06 05:21:31 -0700

    3

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -3,7 +3,7 @@
 /**
  * main - check the code
  *
- * Return: always 0
+ * Return: always 0.
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,6 +5,8 @@
  *
  * Return: always 0
  */
+void print_alphabet_x10(void);
+
 void print_alphabet_10x(void)
 {
 	int i;
