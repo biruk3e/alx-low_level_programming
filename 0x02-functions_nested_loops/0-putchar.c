94ce514101bb4f9facfcf603f46defe04f9edf42
biruk <you@example.com>
2022-07-06 03:40:38 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,9 @@ int main(void)
 	char c[8] = "_putchar";
 	int i;
 
-		for (i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-	       _putchar(c[i]);
+	_putchar(c[i]);
 	}
 	_putchar('\n');
 	return (0);
