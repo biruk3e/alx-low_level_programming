f657c7475d0c277b609fcd90c8e07c112518aa21
biruk <you@example.com>
2022-07-07 03:44:37 -0700

    00

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints puthcar
+ *
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+	return (1);
+	}
+	else 
+	{
+	_putchar('\n');
+	return (0);
+	}
+}
